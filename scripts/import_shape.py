a30555d4f81d3b3da8130870ca9bff0d3e848d0a
migtorres <miguel.torres@unep-wcmc.org>
2016-06-07 17:26:19 +0100

    uses arguments as file names

--- scripts/import_shape.py
+++ scripts/import_shape.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python
 import sys
+import os
 import os.path  
 import psycopg2
 import osgeo.ogr
+from os.path import basename
+import pdb
 
 sys.path.insert(0, '../wcmc_geonode/wcmc_geonode')
 
@@ -18,13 +21,15 @@ serverName = datastore['HOST']
 port = datastore['PORT']
 usr = datastore['USER']
 pw = datastore['PASSWORD']
-sourceFile = '/home/miguel/Downloads/ne_10m_admin_0_countries.shp'
+sourceFile = sys.argv[1]
 conn_str = "PG:dbname='%s' host='%s' port='%s' user='%s' password='%s'" % (database,serverName,port,usr,pw)
 
-def testLoad(serverDS, table, sourceFile):
+def testLoad(serverDS, sourceFile):
     ogr.RegisterAll()
     shapeDS = ogr.Open(sourceFile)
     sourceLayer = shapeDS.GetLayerByIndex(0)
+    file_name = basename(sourceFile)
+    table = os.path.splitext(file_name)[0]
     options = []
     newLayer = serverDS.CreateLayer(table,sourceLayer.GetSpatialRef(),ogr.wkbUnknown,options)
     for x in xrange(sourceLayer.GetLayerDefn().GetFieldCount()):
@@ -45,4 +50,4 @@ def testLoad(serverDS, table, sourceFile):
 
 
 ogrds = ogr.Open(conn_str)
-name = testLoad(ogrds,'countries4',sourceFile)
+name = testLoad(ogrds,sourceFile)
